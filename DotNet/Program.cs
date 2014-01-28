b8908c1b25d0370c8a4b065ec6a6a6f81599badd
Xuegen Jin <xuegen.jin@gmail.com>
2014-01-27 23:36:48 -0500

    Insert test

--- DotNet/Program.cs
+++ DotNet/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace DotNet
 {
@@ -10,7 +9,47 @@ namespace DotNet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var connectionString = "mongodb://localhost";
+            var client = new MongoClient(connectionString);
+
+            var server = client.GetServer();
+            var database = server.GetDatabase("course");
+
+            Insert(database);
+
+            Console.Read();
+        }
+
+        static void Insert(MongoDatabase db)
+        {
+            var collection = db.GetCollection("insertTest");
+
+            collection.Drop();
+
+
+            //insert one doc
+            var doc = new BsonDocument().Add("x", 1);
+            Console.WriteLine(doc);
+
+            collection.Insert(doc);
+
+            Console.WriteLine(doc);
+
+            //batch insert
+            List<BsonDocument> documents = new List<BsonDocument>();
+
+            for (var x = 2; x < 10; x++)
+            {
+                documents.Add(new BsonDocument("x", x));
+            }
+
+            collection.InsertBatch(documents);
+
+            foreach (var d in collection.Find(null))
+            {
+                Console.WriteLine(d);
+            }
+
         }
     }
 }
