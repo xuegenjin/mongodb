6e7bc8033db544c1d50710e12b52abb9a64e1463
Xuegen Jin <xuegen.jin@gmail.com>
2014-01-28 23:10:27 -0500

    insert via domain model

--- DotNet/Program.cs
+++ DotNet/Program.cs
@@ -4,6 +4,7 @@ using System.Configuration;
 
 using MongoDB.Bson;
 using MongoDB.Driver;
+using System.Text;
 
 
 namespace DotNet
@@ -18,12 +19,14 @@ namespace DotNet
             var server = client.GetServer();
             var database = server.GetDatabase(ConfigurationManager.AppSettings["database"]);
 
-            Insert(database);
+            InsertByDocument(database);
+
+            InsertByModel(database);
 
             Console.Read();
         }
 
-        static void Insert(MongoDatabase db)
+        static void InsertByDocument(MongoDatabase db)
         {
             var collection = db.GetCollection("insertTest");
 
@@ -52,6 +55,56 @@ namespace DotNet
                 Console.WriteLine(d);
             }
 
+        }
+
+        public class Coordinate
+        {
+            public ObjectId Id { get; set; }
+            public int x {get; set;}
+
+            public override string ToString()
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append("{ ");
+
+                if (Id != ObjectId.Empty)
+                    sb.AppendFormat("\"_id\" : ObjectId(\"{0}\"), ", Id);
+
+                sb.AppendFormat("\"x\" : {0}", x);
+
+                sb.Append(" }");
+
+                return sb.ToString();
+            }
+        }
+
+        static void InsertByModel(MongoDatabase db)
+        {
+            var collection = db.GetCollection<Coordinate>("insertTest");
+
+            collection.Drop();
+
+            Coordinate c = new Coordinate { x = 1 };
+
+            Console.WriteLine(c);
+
+            collection.Insert(c);
+
+            Console.WriteLine(c);
+
+            List<Coordinate> cs = new List<Coordinate>();
+
+            for (var x = 2; x < 10; x++)
+            {
+                cs.Add(new Coordinate { x = x });
+            }
+
+            collection.InsertBatch(cs);
+
+            foreach(var coord in collection.Find(null))
+            {
+                Console.WriteLine(coord);
+            }
         }
     }
 }
